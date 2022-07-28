e55893d936806d4e720b3f4c2e924deaa3d40dfe
Algolorian <93614894+Algolorian@users.noreply.github.com>
2022-07-28 08:02:08 -0500

    Update

--- main.py
+++ main.py
@@ -1,75 +1,87 @@
-'''
 import pyautogui
 
-screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
+x, y = 20, 50  # setting vars
 
-currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
 
-pyautogui.moveTo(100, 150) # Move the mouse to XY coordinates.
+screenWidth, screenHeight = pyautogui.size()  # Get the size of the primary monitor.
 
+currentMouseX, currentMouseY = pyautogui.position()  # Get the XY position of the mouse.
+
+
+x_width, y_width = (100, 100)  # box size for search
 pyautogui.locateOnScreen('picture.png',
-						 region = (x_screen, y_screen, x_width, y_width),
-						 grayscale = True,
-						 confidence = 0.9)
+                         region=(screenWidth, screenHeight, x_width, y_width),
+                         grayscale=True,
+                         confidence=0.9)
 
 pyautogui.locateCenterOnScreen('picture.png',
-							   region = (x_screen, y_screen, x_width, y_width),
-							   grayscale = True,
-							   confidence = 0.9)
-r,g,b = pyautogui.pixel(x, y)
-
-pic = pyautogui.screenshot(region = (x_screen, y_screen, x_width, y_width))
-
-r,g,b = pic.getpixel(x, y)
+                               region=(screenWidth, screenHeight, x_width, y_width),
+                               grayscale=True,
+                               confidence=0.9)
 
 
-pyautogui.click()          # Click the mouse.
+r1, g1, b1 = pyautogui.pixel(x, y)
+
+pic = pyautogui.screenshot(region=(screenWidth, screenHeight, x_width, y_width))
+
+r2, g2, b2 = pic.getpixel(x, y)
+
+
+pyautogui.click()  # Click the mouse.
 pyautogui.click(100, 200)  # Move the mouse to XY coordinates and click it.
-pyautogui.click('button.png') # Find where button.png appears on the screen and click it.
+pyautogui.click('button.png')  # Find where button.png appears on the screen and click it.
 
-pyautogui.move(0, 10)      # Move mouse 10 pixels down from its current position.
-pyautogui.doubleClick()    # Double click the mouse.
-pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)  # Use tweening/easing function to move mouse over 2 seconds.
+pyautogui.move(0, 10)  # Move mouse 10 pixels down from its current position.
+pyautogui.moveTo(100, 150)  # Move the mouse to XY coordinates.
+pyautogui.doubleClick()  # Double click the mouse.
+pyautogui.moveTo(500, 500, duration=2,
+                 tween=pyautogui.easeInOutQuad)  # Use tweening/easing function to move mouse over 2 seconds.
 
 pyautogui.click(clicks=2)  # double-click the left mouse button
-pyautogui.click(clicks=2, interval=0.25)  # double-click the left mouse button, but with a quarter second pause in between clicks
-pyautogui.click(button='right', clicks=3, interval=0.25)  ## triple-click the right mouse button with a quarter second pause in between clicks
+pyautogui.click(clicks=2,
+                interval=0.25)  # double-click the left mouse button, but with a quarter second pause in between clicks
+pyautogui.click(button='right', clicks=3,
+                interval=0.25)  # triple-click the right mouse button with a quarter second pause in between clicks
 
-pyautogui.dragTo(100, 200, button='left')     # drag mouse to X of 100, Y of 200 while holding down left mouse button
-pyautogui.dragTo(300, 400, 2, button='left')  # drag mouse to X of 300, Y of 400 over 2 seconds while holding down left mouse button
-pyautogui.drag(30, 0, 2, button='right')   # drag the mouse left 30 pixels over 2 seconds while holding down the right mouse button
+pyautogui.dragTo(100, 200, button='left')  # drag mouse to X of 100, Y of 200 while holding down left mouse button
+pyautogui.dragTo(300, 400, 2,
+                 button='left')  # drag mouse to X of 300, Y of 400 over 2 seconds while holding down left mouse button
+pyautogui.drag(30, 0, 2,
+               button='right')  # drag the mouse left 30 pixels over 2 seconds while holding down the right mouse button
 
 
 pyautogui.write('Hello world!', interval=0.25)  # type with quarter-second pause in between each key
-pyautogui.press('esc')     # Press the Esc key. All key names are in pyautogui.KEY_NAMES
+pyautogui.press('esc')  # Press the Esc key. All key names are in pyautogui.KEY_NAMES
 pyautogui.press('left', presses=3)
 
-pyautogui.keyDown('shift') # Press the Shift key down and hold it.
-pyautogui.press(['left', 'left', 'left', 'left']) # Press the left arrow key 4 times.
-pyautogui.keyUp('shift')   # Let go of the Shift key.
+pyautogui.keyDown('shift')  # Press the Shift key down and hold it.
+pyautogui.press(['left', 'left', 'left', 'left'])  # Press the left arrow key 4 times.
+pyautogui.keyUp('shift')  # Let go of the Shift key.
 
 pyautogui.hotkey('ctrl', 'shift', 'esc')
-pyautogui.hotkey('ctrl', 'c') # Press the Ctrl-C hotkey combination.
-'''
+pyautogui.hotkey('ctrl', 'c')  # Press the Ctrl-C hotkey combination.
 
-'''
-The alert() Function
->>> alert(text='', title='', button='OK')
-Displays a simple message box with text and a single OK button. Returns the text of the button clicked on.
 
-The confirm() Function
->>> confirm(text='', title='', buttons=['OK', 'Cancel'])
-Displays a message box with OK and Cancel buttons. Number and text of buttons can be customized. Returns the text of the button clicked on.
+# The alert() Function
+# Displays a simple message box with text and a single OK button.
+# Returns the text of the button clicked on.
+pyautogui.alert(text='', title='', button='OK')
 
-The prompt() Function
->>> prompt(text='', title='' , default='')
-Displays a message box with text input, and OK & Cancel buttons. Returns the text entered, or None if Cancel was clicked.
 
-The password() Function
->>> password(text='', title='', default='', mask='*')
-Displays a message box with text input, and OK & Cancel buttons. Typed characters appear as *. Returns the text entered, or None if Cancel was clicked.
-'''
+# The confirm() Function
+# Displays a message box with OK and Cancel buttons. Number and text of buttons can be customized.
+# Returns the text of the button clicked on.
+pyautogui.confirm(text='', title='', buttons=['OK', 'Cancel'])
 
+# The prompt() Function
+# Displays a message box with text input, and OK & Cancel buttons.
+# Returns the text entered, or None if Cancel was clicked.
+pyautogui.prompt(text='', title='' , default='')
+
+# The password() Function
+# Displays a message box with text input, and OK & Cancel buttons.
+# Typed characters appear as *. Returns the text entered, or None if Cancel was clicked.
+pyautogui.password(text='', title='', default='', mask='*')
 
 '''
 ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
